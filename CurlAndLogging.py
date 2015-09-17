7bc11b42ca10ead88bb20b3a7df893ea18ca48eb
bici-zhuguangzu <bici_zhuguangzu@outlook.com>
2015-09-17 16:58:49 +0800

    修改 CurlAndLogging.py
    访问一个url地址如果成功输出到日志执行成功，如果失败输出到日志文件并打印httpcode

--- CurlAndLogging.py
+++ CurlAndLogging.py
@@ -7,30 +7,36 @@
 
 import pycurl
 import logging
+import StringIO
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d]  \
                     %(levelname)s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename='executeResult.log',
+                    datefmt='%a, %Y-%m-%d %H:%M:%S',
+                    filename='../logs/executeResult.log',
                     filemode='a')  # 指定日志记录方式为追加
 
 
 def testcurl(url):
     curl = pycurl.Curl()
+    output = StringIO.StringIO()
     curl.setopt(pycurl.URL, url)
     curl.setopt(pycurl.CONNECTTIMEOUT, 10)
+    # 把StringIO的写函数注册到pycurl的WRITEFUNCTION中，即pycurl所有获取的内容都写入到StringIO中，如果没有这一句，pycurl就会把所有的内容在默认的输出器中输出
+    curl.setopt(pycurl.WRITEFUNCTION, output.write)
     curl.perform()
+    # output.getvalue()
     return curl.getinfo(curl.HTTP_CODE)  # 返回http code
     curl.close
 
 
 def main():
     url = '192.168.12.210'
-    if testcurl(url) != 200:
-        logging.warning("execute failure")
+    retrunCode = testcurl(url)
+    if retrunCode != 200:
+        logging.warning("access failure with HTTP_CODE %s" % retrunCode)
     else:
-        logging.debug("execute ok")
+        logging.debug("access ok %s" % retrunCode)
 
 
 if __name__ == '__main__':
