377f6cf41cd74948272a0baf3655d0bfef0d0e2f
bici-zhuguangzu <bici_zhuguangzu@otlook.com>
2015-10-30 21:54:05 +0800

    添加test目录移动test项目

--- shell/nginx_status.sh
+++ shell/nginx_status.sh
@@ -6,22 +6,14 @@
 # @Link    : http://blog.zhuguangzu.xyz
 # @Version : $Id$
 
-lan=`ifconfig eth0|grep inet|awk -F " " '{print $2}'|awk -F ":" '{print $2}'` 
+lan=`ifconfig eth0|grep inet|awk -F " " '{print $2}'|awk -F ":" '{print $2}'`
 
 AddConfForNginxStat(){
     cat > status.conf << EOF
-#
-# The default server
-#
 server {
-    listen       $lan:8080;
+    listen       8080;
     server_name  _;
 
-    #charset koi8-r;
-
-    #access_log  logs/host.access.log  main;
-
-    # Load configuration files for the default server block.
     include /etc/nginx/default.d/*.conf;
 
     location / {
@@ -34,35 +26,10 @@ server {
         root   /usr/share/nginx/html;
     }
 
-    # redirect server error pages to the static page /50x.html
-    #
     error_page   500 502 503 504  /50x.html;
     location = /50x.html {
         root   /usr/share/nginx/html;
     }
-
-    # proxy the PHP scripts to Apache listening on 127.0.0.1:80
-    #
-    #location ~ \.php$ {
-    #    proxy_pass   http://127.0.0.1;
-    #}
-
-    # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-    #
-    #location ~ \.php$ {
-    #    root           html;
-    #    fastcgi_pass   127.0.0.1:9000;
-    #    fastcgi_index  index.php;
-    #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
-    #    include        fastcgi_params;
-    #}
-
-    # deny access to .htaccess files, if Apache's document root
-    # concurs with nginx's one
-    #
-    #location ~ /\.ht {
-    #    deny  all;
-    #}
 }
 EOF
 }
