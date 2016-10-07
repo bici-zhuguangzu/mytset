923c86ba936e714d636eb9b59bd3769b85e01db5
guangzu <bici_zhuguangzu@outlook.com>
2016-10-07 17:12:56 +0800

    add 2016.1017
    test for docopt

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-10-06 13:56:13
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+
+print 1
---
+++ www.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-10-06 14:56:52
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+"""Qingchat CLI
+
+Usage:
+    www search group name <group> <name>
+    www search source ip <sourceip>
+    www search lanip ip <lanip>
+Options:
+  -h --help     Show this screen.
+  -v --version     Show version.
+"""
+
+import os
+import time
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    argumemt = docopt(__doc__, version='wwww 0.0.1')
+    print argumemt['<group>']
+    print argumemt['<name>']
